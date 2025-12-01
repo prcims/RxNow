7e30d3a4f167845f5df073ed835e1aec6bab1b39
prcims <cimspr@gmail.com>
2025-12-01 00:15:22 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import RxOrdersApp from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RxOrdersApp />
+  </React.StrictMode>
+);
